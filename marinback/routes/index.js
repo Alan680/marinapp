074086a80471aec58f5100e428dcfb36392b5e0c
Alan <alanmaxit312@gmail.com>
2024-05-26 11:45:42 -0300

    Anda el back y el front

--- marinback/routes/index.js
+++ marinback/routes/index.js
@@ -1,23 +1,20 @@
 import express from 'express';
 const router = express.Router();
 
-const URL = 'http://localhost:3000';
+const URL = 'http://localhost:5173';
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.redirect('/login'); // Redirige a la página de inicio de sesión
 });
 
+router.get('/', (req, res, next) => {
+  res.render('${URL}/login'); // Renderiza la plantilla de la página de inicio de sesión
+});
 /* Redirect to dashboard. */
 router.get('/dashboard', (req, res, next) => {
-  res.redirect('${url}/dashboard');
+  res.redirect('${URL}/dashboard');
 });
 
-router.post('/register', [
-  body('nombre').not().isEmpty().withMessage('El nombre es requerido'),
-  body('apellido').not().isEmpty().withMessage('El apellido es requerido'),
-  body('email').isEmail().withMessage('Introduce un email válido'),
-  body('password').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres')
-], registerUser);
 
 
 
