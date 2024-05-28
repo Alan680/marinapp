90943187488def29bacfd5755bee86e865e44a1d
Alan <alanmaxit312@gmail.com>
2024-05-28 10:38:53 -0300

    Despacho listo

--- marinapp/src/NuevoDespacho.jsx
+++ marinapp/src/NuevoDespacho.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { Header } from './Header';
 import * as React from 'react';
-import { Center, Box, Heading, FormControl, FormLabel, Input, Select, Button } from '@chakra-ui/react';
+import { Center, Box, Heading, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
 
 export function DespachoNew() {
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
     const [despacho, setDespacho] = useState({
         nombreEmbarcacion: '',
         matriculaEmbarcacion: '',
@@ -17,19 +19,42 @@ export function DespachoNew() {
         observaciones: ''
     });
 
-    function handleChange(event, index) {
+    const handleChange = (event, index) => {
         const { name, value } = event.target;
-        const pasajerosActualizados = [...despacho.pasajeros];
-        pasajerosActualizados[index][name] = value;
-        setDespacho({ ...despacho, pasajeros: pasajerosActualizados });
-    }
+        if (name === 'nombre' || name === 'apellido') {
+            const pasajerosActualizados = [...despacho.pasajeros];
+            pasajerosActualizados[index][name] = value;
+            setDespacho({ ...despacho, pasajeros: pasajerosActualizados });
+        } else {
+            setDespacho({ ...despacho, [name]: value });
+        }
+    };
 
-    function handleAddPasajero() {
+    const handleAddPasajero = () => {
         setDespacho({ ...despacho, pasajeros: [...despacho.pasajeros, { nombre: '', apellido: '' }] });
-    }
+    };
 
-    async function handleSubmit(e) {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+
+        const { nombreEmbarcacion, matriculaEmbarcacion, fechaSalida, horaSalida, pasajeros, dniResponsable, telefono, fechaLlegada, horaLlegada, observaciones } = despacho;
+
+        if (
+            nombreEmbarcacion === '' || 
+            matriculaEmbarcacion === '' || 
+            fechaSalida === '' || 
+            horaSalida === '' || 
+            pasajeros.length === 0 || 
+            pasajeros.some(pasajero => pasajero.nombre === '' || pasajero.apellido === '') || 
+            dniResponsable === '' || 
+            telefono === '' || 
+            fechaLlegada === '' || 
+            horaLlegada === '' || 
+            observaciones === ''
+        ) {
+            setError('Por favor, complete todos los campos.');
+            return;
+        }
 
         try {
             const response = await fetch('http://localhost:8080/despacho', {
@@ -37,36 +62,24 @@ export function DespachoNew() {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({
-                    nombreEmbarcacion: despacho.nombreEmbarcacion,
-                    matriculaEmbarcacion: despacho.matriculaEmbarcacion,
-                    fechaSalida: despacho.fechaSalida,
-                    horaSalida: despacho.horaSalida,
-                    pasajeros: despacho.pasajeros,
-                    dniResponsable: despacho.dniResponsable,
-                    telefono: despacho.telefono,
-                    fechaLlegada: despacho.fechaLlegada,
-                    horaLlegada: despacho.horaLlegada,
-                    observaciones: despacho.observaciones
-                }),
+                body: JSON.stringify(despacho),
                 credentials: 'include', // Envia cookies con la solicitud
             });
 
             const data = await response.json();
 
             if (response.ok) {
-                // Si la autenticación es exitosa, guarda el token 
-                sessionStorage.setItem('usuario', socio.email);
-                navigate('/dashboard');
+                setSuccess('Despacho creado con éxito');
+                setError('');
             } else {
-                // Manejar errores de autenticación
-                setError(data.errores || 'Error de autenticación');
+                setError(data.errores || 'Error al crear el despacho');
             }
         } catch (error) {
             console.error('Error de conexión:', error);
             setError('Error de conexión');
         }
-    }
+    };
+
     return (
         <>
             <Header />
@@ -76,52 +89,54 @@ export function DespachoNew() {
                         <Heading>Nuevo despacho</Heading>
                     </Box>
                     <Box p='20px'>
+                        {error && <Box color='red'>{error}</Box>}
+                        {success && <Box color='green'>{success}</Box>}
                         <form onSubmit={handleSubmit}>
                             <FormControl mt='3px'>
                                 <FormLabel>Nombre de la embarcación</FormLabel>
-                                <Input type='text' required onChange={event => setDespacho({ ...despacho, nombreEmbarcacion: event.target.value })} />
+                                <Input name='nombreEmbarcacion' type='text' value={despacho.nombreEmbarcacion} onChange={handleChange} required />
                             </FormControl>
                             <FormControl mt='3px'>
                                 <FormLabel>Matrícula de la embarcación</FormLabel>
-                                <Input type="text" required onChange={event => setDespacho({ ...despacho, matriculaEmbarcacion: event.target.value })} />
+                                <Input name='matriculaEmbarcacion' type="text" value={despacho.matriculaEmbarcacion} onChange={handleChange} required />
                             </FormControl>
                             <FormControl mt='3px'>
                                 <FormLabel>Fecha de salida</FormLabel>
-                                <Input type="date" required onChange={event => setDespacho({ ...despacho, fechaSalida: event.target.value })} />
+                                <Input name='fechaSalida' type="date" value={despacho.fechaSalida} onChange={handleChange} required />
                             </FormControl>
                             <FormControl mt='3px'>
                                 <FormLabel>Hora de salida</FormLabel>
-                                <Input type="time" required onChange={event => setDespacho({ ...despacho, horaSalida: event.target.value })} />
+                                <Input name='horaSalida' type="time" value={despacho.horaSalida} onChange={handleChange} required />
                             </FormControl>
                             <Box mt='3px'>
                                 <FormLabel>Pasajeros</FormLabel>
                                 {despacho.pasajeros.map((pasajero, index) => (
                                     <Box key={index} display='flex' mt='3px'>
-                                        <Input type='text' name='nombre' placeholder='Nombre' value={pasajero.nombre} onChange={event => handleChange(event, index)} />
-                                        <Input type='text' name='apellido' placeholder='Apellido' value={pasajero.apellido} onChange={event => handleChange(event, index)} />
+                                        <Input name='nombre' placeholder='Nombre' value={pasajero.nombre} onChange={event => handleChange(event, index)} required />
+                                        <Input name='apellido' placeholder='Apellido' value={pasajero.apellido} onChange={event => handleChange(event, index)} required />
                                     </Box>
                                 ))}
                                 <Button mt='10px' onClick={handleAddPasajero}>Agregar pasajero</Button>
                             </Box>
                             <FormControl mt='3px'>
                                 <FormLabel>Número de DNI del responsable</FormLabel>
-                                <Input type="text" required onChange={event => setDespacho({ ...despacho, dniResponsable: event.target.value })} />
+                                <Input name='dniResponsable' type="text" value={despacho.dniResponsable} onChange={handleChange} required />
                             </FormControl>
                             <FormControl mt='3px'>
                                 <FormLabel>Número de teléfono</FormLabel>
-                                <Input type="text" required onChange={event => setDespacho({ ...despacho, telefono: event.target.value })} />
+                                <Input name='telefono' type="text" value={despacho.telefono} onChange={handleChange} required />
                             </FormControl>
                             <FormControl mt='3px'>
                                 <FormLabel>Fecha de llegada</FormLabel>
-                                <Input type="date" required onChange={event => setDespacho({ ...despacho, fechaLlegada: event.target.value })} />
+                                <Input name='fechaLlegada' type="date" value={despacho.fechaLlegada} onChange={handleChange} required />
                             </FormControl>
                             <FormControl mt='3px'>
                                 <FormLabel>Hora de llegada</FormLabel>
-                                <Input type="time" required onChange={event => setDespacho({ ...despacho, horaLlegada: event.target.value })} />
+                                <Input name='horaLlegada' type="time" value={despacho.horaLlegada} onChange={handleChange} required />
                             </FormControl>
                             <FormControl mt='3px'>
                                 <FormLabel>Observaciones</FormLabel>
-                                <Input type="text" required onChange={event => setDespacho({ ...despacho, observaciones: event.target.value })} />
+                                <Input name='observaciones' type="text" value={despacho.observaciones} onChange={handleChange} required />
                             </FormControl>
                             <FormControl mt='3px'>
                                 <Button type="submit">Crear despacho</Button>
